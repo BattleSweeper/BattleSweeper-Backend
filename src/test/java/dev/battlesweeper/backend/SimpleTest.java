60f863c95f2d7efbef93708e32fd9c8c0385b15d
mooner1022 <siwol@mooner.dev>
2023-05-15 12:57:33 +0900

    Update SimpleTest.java

--- src/test/java/dev/battlesweeper/backend/SimpleTest.java
+++ src/test/java/dev/battlesweeper/backend/SimpleTest.java
@@ -3,6 +3,7 @@ package dev.battlesweeper.backend;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.battlesweeper.backend.auth.EmailSender;
+import dev.battlesweeper.backend.objects.Position;
 import dev.battlesweeper.backend.socket.packet.GameFoundPacket;
 import dev.battlesweeper.backend.socket.packet.Packet;
 import dev.battlesweeper.backend.socket.packet.TestPacket;
@@ -20,11 +21,11 @@ public class SimpleTest {
     @Test
     void testSerializer() throws JsonProcessingException {
         var mapper = new ObjectMapper();
-        var serialized = mapper.writeValueAsString(new GameFoundPacket(UUID.randomUUID(), "123123"));
-        System.out.println(serialized);
-        GameFoundPacket packet = (GameFoundPacket) mapper.readValue(serialized, Packet.class);
-        System.out.println(packet.authToken);
-        System.out.println(mapper.writeValueAsString(new TestPacket("World!")));
+        var position = new Position(10, 20);
+        var encoded = mapper.writeValueAsString(position);
+        System.out.println(encoded);
+        var decoded = mapper.readValue(encoded, Position.class);
+        System.out.println(decoded);
     }
 
     @Test
