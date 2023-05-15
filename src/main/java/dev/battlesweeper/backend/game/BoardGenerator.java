fe75b3af73735a51d54f6b4ac64d9f6b18ddeddf
mooner1022 <siwol@mooner.dev>
2023-05-15 18:48:23 +0900

    Add BoardGenerator(Incomplete)

---
+++ src/main/java/dev/battlesweeper/backend/game/BoardGenerator.java
@@ -0,0 +1,12 @@
+package dev.battlesweeper.backend.game;
+
+import dev.battlesweeper.backend.objects.Position;
+
+public class BoardGenerator {
+    private BoardGenerator() {}
+
+    public static Position[] generateMines(Position boardSize) {
+        // TODO: Complete board generation algorithm
+        return null;
+    }
+}
